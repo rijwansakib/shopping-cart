8ec3a58b036b1f3ee1e346e506ca75225f202470
rijwan <rijwansakib1@gmail.com>
2021-09-14 20:17:35 +0600

    btn color by  bootstrap

--- js/app.js
+++ js/app.js
@@ -30,8 +30,8 @@ const showProducts = (products) => {
 
 
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
+      <button id="details-btn" class="btn btn-warning">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
